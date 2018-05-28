35b7241cc9aad293dd23ee9d4f68169d986c702a
Adrian Aley <adrian@aleyanalytics.com>
2018-05-27 21:24:53 -0400

    Update to_db.py

--- to_db.py
+++ to_db.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 
 # to_db.py
-__author__ = 'adrian aley'
 
 import MySQLdb as mdb # MySQL server and database storage
 from pandas.io import sql
 
 # obtain a database connection to the mysql instance
-db_host = 'localhost'
+db_host = 'hostname'
 db_user = 'username'
 db_pass = 'password'
-db_name = 'stock_master'
+db_name = 'database'
 con = mdb.connect(db_host, db_user, db_pass, db_name)
 
 df.to_sql(con=con, name='stock_data', if_exists='replace', flavor='mysql')
