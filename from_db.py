98f21683fac74500ade0fcdac53215865bf0f50d
Adrian Aley <adrian@aleyanalytics.com>
2018-05-27 21:26:11 -0400

    Update from_db.py

--- from_db.py
+++ from_db.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
 # from_db.py
-__author__ = 'adrian aley'
 
 import MySQLdb as mdb # MySQL server and database storage
 
 # obtain a database connection to the MySQL instance
-db_host = 'localhost'
+db_host = 'hostname'
 db_user = 'username'
 db_pass = 'password'
-db_name = 'stock_master'
+db_name = 'database'
 con = mdb.connect(db_host, db_user, db_pass, db_name)
 
 def ticker_pull():
