ccffea6dc8e781f293293be3cec52262640481f8
Adrian Aley <adrian@aleyanalytics.com>
2018-10-23 07:32:29 -0400

    Update stock_analysis.py

--- stock_analysis.py
+++ stock_analysis.py
@@ -3,73 +3,80 @@
 
 import os, sys # operating system interface
 # import from_db as fdb # MySQL instance
-import datetime as dt, time # date & time
-from pandas.tseries import holiday, offsets # U.S. business (trading) days
+import datetime as dt # date & time
 import tech_analysis as tech # technical indicators
 import pandas as pd, numpy as np # data analysis & wrangling
+import matplotlib.pyplot as plt  # plotting data
+import seaborn as sns # statistical data visualization
+sns.set_style('darkgrid')
 from tradingWithPython.lib import yahooFinance as yf # historical financial data
 
-"""Add symbols manually or S&P500 symbols from MySQL instance"""
-symbols = ['WFC','ORCL','JNJ','MSFT','VZ','DIS','WMT','MCD','TGT','COKE'] # add tickers
+symbols = ['WFC','ORCL','JNJ','MSFT','VZ','DIS','WMT','MCD','TGT','COKE','FB','SNAP']
 # symbols = fdb.symbols[8:16] # pull symbols directly from database
-
-print("Python: {}\n".format(sys.version)) # python version & enviroment
 
 start = 2000/1/1 # set starting date for historic data
 tz = os.environ['TZ']='America/New_York' # set local timezone
 today = dt.date.today() # current day
 now = dt.datetime.now() # current time
+count = len(symbols) # ticker list size
 
-stock_count = len(symbols) # ticker list size
-stock_size = int(stock_count/2)
+print ("Python: {}\n\n".format(sys.version) + tz + 
+       '\n' + now.strftime('%I:%M:%S%p') + ' ' + today.strftime(
+        '%m/%d/%Y') + '\n\n' + 'Downloading Data for %s Stocks...\n' % count)
 
-# create series of all business (trading) days
-us_bd = offsets.CustomBusinessDay(calendar=holiday.USFederalHolidayCalendar())
-bdays = pd.Series(pd.date_range(start, today, freq=us_bd, name='Date'))
+stocks = {s: yf.getHistoricData(
+        s, start) for s in symbols} # fetch price data for given tickers
+stocks = {s: tech.analyze(
+        stocks[s]) for s in symbols} # append technical indicators
 
-print (tz + '\n' + now.strftime('%I:%M:%S%p') + ' ' + today.strftime('%m/%d/%Y') + '\n\n' + 
-       'Downloading Data for %s Stocks...\n' % stock_count)
-
-try:
-    stocks = {s: yf.getHistoricData(s, start) for s in symbols}
-    stocks = {s: tech.analyze(stocks[s]) for s in symbols}
-except Exception as e:
-    pass
-
-elapsed = dt.datetime.now() - now
-print(('\n' 'Completed Successfully in %s') % elapsed)
-
-for s in symbols:
+for s in symbols: # format date (index) column
     stocks[s]["Date"] = stocks[s].index.date
     stocks[s].set_index("Date", drop=True, inplace=True)
-    
-close = pd.DataFrame({s: stocks[s]['adj_close'] for s in symbols})
-closefill = close.fillna(1)
 
-# normalize adjusted close price for all stocks (scale: 1-100)
+elapsed = dt.datetime.now() - now # calculate total execution time
+print(('\n' 'Completed Successfully in %s') % elapsed)
+  
+close = pd.DataFrame({s: stocks[s]['adj_close'] for s in symbols})
+price = {s: close.loc[:, s] for s in symbols}
+close = close.dropna()
+missing_cl = close.isna()
+info = close.describe()
+
+# normalize adjusted close price for all stocks (rescaled: 0-1)
+low_cl = close.min()
+lowest_cl = low_cl.min()
 high_cl = close.max()
 highest_cl = high_cl.max()
-normalized = (close/highest_cl) * 100
+normalized = (close - lowest_cl) / (highest_cl - lowest_cl)
+ 
+first_cl = {s: stocks[s]['adj_close'].
+            head(1).transpose() for s in symbols} # first traded price within range
+last_cl = close.tail(1).transpose() # last traded price within range
 
-# get the first & last traded price for each stock
-first_close = close.head(1).transpose()
-last_close = close.tail(1).transpose()
-
-# calculate up days, cummulative return & daily price change for each stock
-daily_return = closefill.apply(lambda x: x / x[0])
+# calculate correlation, cummulative return & daily price change for each stock
+daily_return = close.apply(lambda x: x / x[0])
 stock_change = close.apply(lambda x: np.log(x) - np.log(x.shift(1)))
-stock_change = stock_change.fillna(0)
 dataDiff = close.diff()[1:]
 countPositive = dataDiff > 0
 upDays = pd.DataFrame(countPositive.sum())
 total_return = daily_return.tail(1).transpose()
-        
+corr = close.corr()
+
+# create csv files for each stock
+cwd = os.path.dirname(__file__)
+for s in symbols:
+    stocks[s].to_csv(cwd + '/dumps/' + s + '.csv')
+
 # start plotting data
 daily_return.plot(grid = True, title = 'Cummulative Return (Percent)', lw = .5
                   ).axhline(y = 1, color = "black", lw = 2)
 stock_change.plot(grid = True, title = 'Price Change (Percent)', lw = .5
                   ).axhline(y = 0, color = "black", lw = 2)
-close.plot(grid = True, subplots=True, title = 'Adjusted Close (Seperated)', 
-           lw = .5, layout=(stock_size, 2))
+stock_change.plot(grid = True, subplots=True, title = 
+                  'Price Change (Percent)', lw = .5)
+close.plot(grid = True, subplots=True, title = 'Adjusted Close (Price)', 
+           lw = .5, layout=(int(count/2), 2))
 close.plot(grid = True, title = 'Adjusted Close (Price)', lw = .5)
-normalized.plot(grid = True, title = 'Adjusted Close (Normalized)', lw = .5)
+_, ax = plt.subplots() 
+_ = sns.heatmap(corr, ax=ax, xticklabels=corr.columns.values, 
+                yticklabels=corr.columns.values, cmap='coolwarm')
